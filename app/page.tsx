56a9c2bd60dabb4cdcc8cf2073ce38bc3634048a
Thitipan010 <6821901011@cdti.ac.th>
2026-06-04 09:25:07 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h3>ยินดีต้อนรับเข้าสู่เว็บ/</h3>
+      <h3>นายฐิติพันธ์/</h3>
+    </div>
+  );
+}
