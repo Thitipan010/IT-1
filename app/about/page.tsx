e4176a7805d0717b799a3386c37ce4b1ccb7288b
Thitiphan010 <6821901011@cdti.ac.th>
2026-07-08 15:04:57 +0700

    Fix merge conflict

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div >
-<<<<<<< HEAD
+
     
        <h2> เกี่ยวกับเรา</h2>
        <p> โดย นายฐิติพันธ์ </p>
@@ -9,13 +9,3 @@ export default function About() {
   </div>
   );
 }
-=======
-     
-      <h3>เกี่ยวกับเรา/</h3>
-      <p>นายฐิติพันธ์/</p>
-
-    
-    </div>
-  );
-}
->>>>>>> c6134f72d86e03045b5da7c736f6533ada1b0273
