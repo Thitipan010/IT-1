32ff9a4f229e3ad471bcb73031c4c7e7d8ee862e
Thitiphan010 <6821901011@cdti.ac.th>
2026-07-08 14:13:08 +0700

    Initial commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() { 
+  return (
+    <div >
+     
+      <h3>ติดต่อเรา/</h3>
+      <p>ร้านไก่ทอดจำกัด/</p>
+
+    
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h2>ยินดีต้อนรับเข้าสู่  ของผมครับ</h2>
+        <p>โดย นายฐิติพันธ์ รักษ์แสงสว่าง </p>
+    </div>
+  );
+}
