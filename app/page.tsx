9a6d7934e2f4f50a7091074bf1b898f72f9ec46d
Thitipan010 <6821901011@cdti.ac.th>
2026-08-27 10:02:15 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import BlogsCard from "@/components/BlogCard";
 
 export default function Home() {
   return (
-    <div>
-        <h2>ยินดีต้อนรับเข้าสู่  ของผมครับ</h2>
-        <p>โดย นายฐิติพันธ์ รักษ์แสงสว่าง </p>
-        
-        <BlogsCard/>
+    <div >
+    
+      <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
+      <p> โดยนายฐิติพันธ์ </p>
+ 
     </div>
   );
 }
